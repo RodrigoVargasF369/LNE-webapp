8c268df97ce6679ef3d7cbb27cfd7b41d3f03cf6
Rodrigo Vargas <rodrigovargas2520@gmail.com>
2024-07-10 12:47:30 -0300

    aws config 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {Amplify} from 'aws-amplify'
-import awsmobile from './aws-exports'
+import {awsmobile} from './aws-exports'
 
 Amplify.configure(awsmobile)
 
