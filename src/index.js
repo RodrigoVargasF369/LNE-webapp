c22aeaa04c7d6d462f57e43d1f80de42988be2c6
Rodrigo Vargas <rodrigovargas2520@gmail.com>
2024-07-10 12:44:18 -0300

    aws config

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import {Amplify} from 'aws-amplify'
-// import config from './aws-exports'
+import {Amplify} from 'aws-amplify'
+import awsmobile from './aws-exports'
 
-// Amplify.configure(config)
+Amplify.configure(awsmobile)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
