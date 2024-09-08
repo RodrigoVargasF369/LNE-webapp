6ad37432f218efca9841d4eca1e92db649cc87fb
Rodrigo Vargas <rodrigovargas2520@gmail.com>
2024-09-08 19:17:44 -0300

    Cambios en home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,17 +5,16 @@ function Home(){
         <div>
             <header className="">
                 <div className="card border-0 rounded-0">
-                  <img src={`${process.env.PUBLIC_URL}/images/banner_loyalty(1920×600).png`} 
+                  <img src={`${process.env.PUBLIC_URL}/images/banner_2.jpg`} 
                   className="card-img rounded-0" alt="..." style={{
-                    maxHeight: '340px', 
                     width: '100%', 
                     objectFit: 'cover', 
                     objectPosition: 'center'
                   }}/>
                   <div className="card-img-overlay d-flex flex-column justify-content-end align-items-start">
-                    <h5 className="card-title text-dark">Elegí tu propio estilo</h5>
+                    {/* <h5 className="card-title text-dark">Elegí tu propio estilo</h5> */}
                     {/* <p className="card-text text-dark">Descripcion</p> */}
-                    <Link to='/productos' className="btn btn-light mt-3">Ver nuestras remeras <i className="bi bi-box-arrow-up-right"></i></Link>
+                    <Link to='/productos' className="btn btn-light mt-3">Ver remeras <i className="bi bi-box-arrow-up-right"></i></Link>
                   </div>
                 </div>
             </header>
@@ -64,9 +63,28 @@ function Home(){
                 </div>
             </section> */}
 
+            <section className="container mt-5">
+                <Link to='/productos'>
+                <div className="row g-5">
+                    <div className="col-1 col-md-3">
+                        <img src={`${process.env.PUBLIC_URL}/images/diseno_2.png`}  alt="remera_diseno_2" 
+                        style={{maxHeight: '480px'}}/>
+                    </div>
+                    <div className="col-1 col-md-3">
+                        <img src={`${process.env.PUBLIC_URL}/images/diseno_3.png`}  alt="remera_diseno_3" 
+                        style={{maxHeight: '500px'}}/>
+                    </div>
+                    <div className="col-1 col-md-3">
+                        <img src={`${process.env.PUBLIC_URL}/images/diseno_1.png`}  alt="remera_diseno_1" 
+                        style={{maxHeight: '480px'}}/>
+                    </div>
+                </div>
+                </Link>
+            </section>
+
             <section className="py-5 ms-3 fixed-bottom">
-                <div className="container text-start">
-                    <div className="row">
+                <div className="container">
+                    <div className="row justify-content-end me-3">
                         <div className="col-1"><a href="https://www.instagram.com/loyalty.clothess" target="_blank" rel="noreferrer noopener"><i className="bi bi-instagram text-dark"></i></a></div>
                         <div className="col-1"><a href="https://www.tiktok.com/@loyaltycordoba" target="_blank" rel="noreferrer noopener"><i className="bi bi-tiktok text-dark"></i></a></div>
                     </div>
